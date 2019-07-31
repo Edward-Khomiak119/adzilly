1bf7d3e3b943d7e075d937ca83df5de2f6e9a1c6
YOUR NAME <YOUR@EMAIL.com>
2019-07-31 15:09:20 +0300

    all project in the bitbucket

---
+++ app/models/rental_property.rb
@@ -0,0 +1,36 @@
+class RentalProperty < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode
+  has_many_attached :images
+  has_many :photos
+  belongs_to :user
+
+  def self.filter_on_constraints(constraint_hash)
+    records = RentalProperty.all
+    operator_hash = {
+        :bedrooms => '>=',
+        :beds => '>=',
+        :maxpersons => '>=',
+        :bathrooms => '>=',
+        :pets_allowed => '==',
+        :price => '<=',
+    }
+    constraint_hash.each_pair do |key, value|
+      symbol = key.to_sym
+      operator = operator_hash[symbol]
+      if !operator.nil?
+        records = records.where("#{symbol.to_s} #{operator} ?", value)
+      end
+    end
+    records
+  end
+
+  def distance_from(location)
+    if location.nil?
+      nil
+    else
+      self.distance_to(location)
+    end
+  end
+
+end
---
+++ db/migrate/20170904184345_create_rental_properties.rb
@@ -0,0 +1,17 @@
+class CreateRentalProperties < ActiveRecord::Migration[5.1]
+  def change
+    create_table :rental_properties do |t|
+      t.string :title
+      t.text :description
+      t.text :address
+      t.decimal :price, precision: 10, scale: 2, null: false
+      t.decimal :latitude, precision: 15, scale: 10, null: false
+      t.decimal :longitude, precision: 15, scale: 10, null: false
+      t.string :email
+      t.string :industry
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
